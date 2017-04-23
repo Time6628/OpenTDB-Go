b43ae561461bb267e03f1348506d42a4d2361821
Time6628 <pturchan@yahoo.com>
2017-04-23 15:37:56 -0400

    spelling error

--- structs.go
+++ structs.go
@@ -20,7 +20,7 @@ type Results struct {
 	Type string `json:"type"`
 	Difficulty string `json:"difficulty"`
 	Question string `json:"question"`
-	CorrectAnswer string `json:"correct_answers"`
+	CorrectAnswer string `json:"correct_answer"`
 	IncorrectAnswer []string `json:"incorrect_answers"`
 }
 
